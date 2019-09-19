1d99306d4d603228dc952cfc79406c4bd3051e86
rick <rickwestdev@gmail.com>
2019-09-19 21:50:37 +0100

    Define message/user relationships

--- app/Message.php
+++ app/Message.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
+    /**
+     * @var array
+     */
     protected $fillable = ['message'];
+
+
+    /**
+     * @return BelongsTo
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
